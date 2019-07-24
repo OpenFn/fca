e172b0ded8b77ded487e9f6364df1a9044864160
Taylor Downs <downs.taylor@gmail.com>
2019-07-24 10:34:43 +0200

    try CSV header syntax

--- fca_bulk_financials.js
+++ fca_bulk_financials.js
@@ -1,7 +1,6 @@
 bulk('ampi__Financial__c', 'upsert', { failOnError: true, extIdField: 'Name'}, state => {
   return state.data.entries.map(line => {
     return {
-      // "SomeExternalId": "blah",
       Name: line.EntryNo_,
       Posting_Date__c: line.PostingDate,
       Account_Number__c: line.G_LAccountNo_,
@@ -13,18 +12,16 @@ bulk('ampi__Financial__c', 'upsert', { failOnError: true, extIdField: 'Name'}, s
       Credit_Amount__c: line.CreditAmount,
       Project_Series__c: line.ProjectSeries,
       Staff_Code__c: line.StaffCode,
+
       //Q2***HOW TO MAP RELATIONSHIP FIELDS IN BULK FUNCTION?*****
-      // ampi__Budget__c: 'a031j00000DxVf8AAF',
-      //IDEAL OUTPUT IN REQUEST BODY:
-      ampi__Budget__r: { Name: 'FCA Nav Default Budget' },
-      //IF WE WERE TO WRITE USING HELPER FUNCTIONS:
-      //relationship('ampi__Budget__r', 'Name', 'FCA NAV Default Budget'),
+      // ampi__Budget__c: 'a031j00000DxVf8AAF', // hard coded
+      'ampi__Budget__r.Name': 'FCA Nav Default Budget', // ideal
 
-      ampi__Reporting_Period__c: 'a0X1j000000MBggEAG',
-      //IDEAL OUTPUT: 'ampi__Reporting_Period__r': {'Name' : 'RP-00020' },
+      // ampi__Reporting_Period__c: 'a0X1j000000MBggEAG', // hard coded
+      'ampi__Reporting_Period__r.Name': 'RP-00020', // ideal
 
-      Project_Number__c: 'a0V1j000000R4D9EAK'
-      //IDEAL OUTPUT: 'Project_Number__r': { 'Project_Number_Auto__c' : line.ProjectNr }
+      // Project_Number__c: 'a0V1j000000R4D9EAK', // hard coded
+      'Project_Number__r.Project_Number_Auto__c': line.ProjectNr, // ideal
     }
   })
 });
