5cab241fcd8164c8bbc0ac3b276f16a9e414edb4
Taylor Downs <downs.taylor@gmail.com>
2019-07-29 11:42:52 +0100

    group and sum entries by project number

--- fca_bulk_financials.js
+++ fca_bulk_financials.js
@@ -26,7 +26,36 @@ bulk(
         };
       });
 
-    const aggregated = [1, 2, 3];
+    const toAggregate = state.data.entries.filter(item => {
+      return item.G_LAccountNo_ < 3310 || item.G_LAccountNo_ > 3888;
+    });
+
+    var aggregated = [];
+    toAggregate.reduce((accumulator, currentItem) => {
+      if (!accumulator[currentItem.ProjectNr]) {
+        accumulator[currentItem.ProjectNr] = {
+          Name: currentItem.EntryNo_,
+          Posting_Date__c: currentItem.PostingDate,
+          Account_Number__c: currentItem.G_LAccountNo_,
+          Account_Name__c: currentItem.AccountName,
+          Document_Number__c: currentItem.DocumentNo_,
+          ampi__Description__c: currentItem.Description,
+          ampi__Amount_Actual__c: 0,
+          Debit_Amount__c: 0,
+          Credit_Amount__c: 0,
+          Project_Series__c: currentItem.ProjectSeries,
+          Staff_Code__c: currentItem.StaffCode,
+          'ampi__Budget__r.Name': 'FCA Nav Default Budget',
+          'ampi__Reporting_Period__r.Name': 'RP-00020',
+          'Project_Number__r.Project_Programme_Number_External_ID__c': currentItem.ProjectNr,
+        };
+        aggregated.push(accumulator[currentItem.ProjectNr]);
+      }
+      accumulator[currentItem.ProjectNr].ampi__Amount_Actual__c += currentItem.Amount;
+      accumulator[currentItem.ProjectNr].Debit_Amount__c += currentItem.DebitAmount;
+      accumulator[currentItem.ProjectNr].Credit_Amount__c += currentItem.CreditAmount;      
+      return accumulator;
+    }, {});
 
     return raw.concat(aggregated);
   }
